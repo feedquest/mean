92604e0dc432ee5396a2e1826690a8d08347cda4
feedquest <patliang@gmail.com>
2016-08-17 13:04:11 -0400

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -7,9 +7,9 @@ module.exports = {
     certificate: './config/sslcerts/cert.pem',
     caBundle: './config/sslcerts/cabundle.crt'
   },
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 8443,
   // Binding to 127.0.0.1 is safer in production.
-  host: process.env.HOST || '127.0.0.1',
+  host: process.env.HOST || '0.0.0.0',
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
